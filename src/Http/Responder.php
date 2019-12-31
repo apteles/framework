501317af858b4aec30012f6778bda190c4a1a58f
Andre Teles <andre.telestp@gmail.com>
2019-12-31 17:31:32 -0300

    finished responder without tests

---
+++ src/Http/Responder.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace ApTeles\Http;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Responder
+{
+    public function __invoke($action, $params)
+    {
+        $this->mergeStreamInputWithGlobalPost();
+
+        $request = $this->createRequest($params);
+
+        print \call_user_func($action, [$request, new Response()]);
+    }
+
+
+    private function mergeStreamInputWithGlobalPost(): void
+    {
+        \parse_str(\file_get_contents('php://input'), $_POST);
+    }
+
+    private function createRequest(array $data = []): Request
+    {
+        return new Request(
+            $_GET,
+            $_POST,
+            \array_merge([], $data),
+            $_COOKIE,
+            $_FILES,
+            $_SERVER
+        );
+    }
+}
