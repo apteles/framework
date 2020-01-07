5831bd42b032d2becf1d85613a54af9036d4a009
Andre Teles <andre.telestp@gmail.com>
2020-01-07 18:29:18 -0300

    refactored to support dependency injection

--- src/Router/ControllerInvoker.php
+++ src/Router/ControllerInvoker.php
@@ -3,36 +3,28 @@ declare(strict_types=1);
 
 namespace ApTeles\Router;
 
+use ApTeles\Http\RequestInterface;
+use ApTeles\Http\ResponseInterface;
 use Invoker\InvokerInterface;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
 
 class ControllerInvoker
 {
     private $invoker;
 
-    private $request;
-
-    private $response;
-
-    public function __construct(InvokerInterface $invoker, ServerRequestInterface $request = null, ResponseInterface $response = null)
+    public function __construct(InvokerInterface $invoker)
     {
         $this->invoker = $invoker;
-        $this->request = $request;
-        $this->response = $response;
     }
 
     public function __invoke(
         callable $callable,
-        array $arguments
-    ) :ResponseInterface {
+        RequestInterface $request = null,
+        ResponseInterface $response = null
+    ) {
         $parameters = [
-            'request' => $this->request,
-            'response' => $this->response
+            'request' => $request,
+            'response' => $response
         ];
-
-        $parameters += $arguments;
-        $parameters += $this->request->getAttributes();
 
         return $this->invoker->call($callable, $parameters);
     }
