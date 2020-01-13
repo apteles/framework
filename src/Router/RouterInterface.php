c012bb0f7d46305928e797b4ce1a26adaa40fba0
Andre Teles <andre.telestp@gmail.com>
2020-01-13 11:30:44 -0300

    updated interface

--- src/Router/RouterInterface.php
+++ src/Router/RouterInterface.php
@@ -5,7 +5,7 @@ namespace ApTeles\Router;
 
 interface RouterInterface
 {
-    public function add(string $method, string $pattern, callable $callback): void;
+    public function add(string $method, string $pattern, callable $callback, string $name = ''): void;
 
-    public function run();
+    public function run(string $httpMethod, string $currentURI);
 }
